f4f0705e361c8b23e9ed7d204a143301f41a4aed
Erkam KUCET <ekucet@gmail.com>
2020-01-28 19:19:39 +0300

    Initial Commit

---
+++ SwiftUICoor/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUICoor
+//
+//  Created by Erkam Kucet on 28.01.2020.
+//  Copyright © 2020 Erkam. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
